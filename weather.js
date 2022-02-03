74a554724d910facb4a1f1def630b43ef2043dc0
Xayrulloh <xayrullohabduvohidov713@gmail.com>
2022-02-03 16:03:09 +0500

    updated

--- weather.js
+++ weather.js
@@ -3,14 +3,13 @@ cityinput.onkeyup = (element) => {
         fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityinput.value+'&appid=8cd377b72bef7c7412b5eb5a005ebbe9')
             .then(res => res.json())
             .then(data => {
-                [cityName.innerHTML, temp.innerHTML, description.innerHTML, wind.innerHTML] = [`Weather of <span>${data['name']}<span>`, `Temperature: <span>${ (data['main']['temp'] - 273).toFixed(2)} C</span>`, `Sky Conditions: <span>${data['weather']['0']['description']}<span>`, `Wind Speed: <span>${data['wind']['speed']} km/h<span>`]
+                [cityName.innerHTML, temp.innerHTML, description.innerHTML, wind.innerHTML, cityinput.value] = [`Weather of <span>${data['name']}<span>`, `Temperature: <span>${ (data['main']['temp'] - 273).toFixed(2)} C</span>`, `Sky Conditions: <span>${data['weather']['0']['description']}<span>`, `Wind Speed: <span>${data['wind']['speed']} km/h<span>`, null]
             })
             .catch(error => {alert('Wrong city name'); [cityName.innerHTML, temp.innerHTML, description.innerHTML, wind.innerHTML, cityinput.value] = [null, null, null, null, null]})
     }
 }
                 
 
-    
 
 
 
